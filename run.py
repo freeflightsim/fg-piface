a9ac6a74d51eea73ad58df73762db635172a54d0
Pedro <pete@daffodil.uk.com>
2014-08-15 06:51:56 +0100

    python to test

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind( ("192.168.5.14", 4567) )
+sock.bind( ("0.0.0.0", 4567) )
 
 while True:
 	data, addr = sock.recvfrom(1024)
