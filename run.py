2de8c1054757c642fabbbbdb255b593bd2f23349
Pete Morgan <pedromorgan@gmail.com>
2014-08-15 09:44:48 +0000

    wtf is appening

--- run.py
+++ run.py
@@ -3,17 +3,18 @@
 import socket
 import pifacedigitalio 
 import time
-import threading
+import thread
+import sys
+import signal
+
+exitapp = False
 
 pif = pifacedigitalio.PiFaceDigital(init_board=True)
 
 
 port  = 45670
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind( ("", port) )
 
-print "listenting on", port
 
 class LED:
 	ap = 0 # auto pilot light
@@ -30,7 +31,16 @@ listener = pifacedigitalio.InputEventListener(chip=pif)
 for i in range(0, 4):
 	listener.register(i, pifacedigitalio.IODIR_FALLING_EDGE, x_cb)
 
-def udp_listen():
+
+
+def run_client():
+
+	global port
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	sock.bind( ("",
+ port) )
+
+	print "listenting on", port
 	c = 0
 	while True:
 		data, addr = sock.recvfrom(1024)
@@ -41,18 +51,44 @@ def udp_listen():
 		
 		flags = [int(p) for p in parts]
 	
+
+
 		pif.output_pins[LED.ap].value = 1 if flags[0]  == 1 else 0
 		pif.output_pins[LED.at].value = 1 if flags[1]  == 1 else 0
 		pif.output_pins[LED.hdg_hold].value = 1 if flags[2]  == 1 else 0
 
 
-t = threading.Thread(target=udp_listen)
-t.setDaemon(True)
-t.start()
+
+#t = threading.Thread(target=udp_listen)
+
+
+def start():
+	threads = []
+	t = UDPClient()
+	t.start()
+	threads.append(t)
+
+	for tr in threads:
+		tr.join()
+
+def cleanup():
+	print "cleanup"
+	#sys.exit(0)
+
+
+
 
 listener.activate()
 
+try:
+	thread.start_new_thread(run_client, ())
+	#run_client()
+except Exception, e:
+	print e
 
-
-
-	
+"""
+if __name__ == "__main__":
+	signal.signal(signal.SIGINT, cleanup)
+	signal.signal(signal.SIGTERM, cleanup)
+	start()	
+"""	
